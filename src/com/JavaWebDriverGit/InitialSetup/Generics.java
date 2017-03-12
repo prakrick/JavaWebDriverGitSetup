c272bb9a5afc23fb388437fd748e786f66cdc7f6
prakrick <prakrick@gmail.com>
2017-03-12 10:31:06 +0530

    Reinit repo

---
+++ src/com/JavaWebDriverGit/InitialSetup/Generics.java
@@ -0,0 +1,10 @@
+package com.JavaWebDriverGit.InitialSetup;
+
+public class Generics {
+//https://www.slideshare.net/eneldoserrata/git-49020118
+	//https://git-scm.com/book/en/v2/
+	//https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
