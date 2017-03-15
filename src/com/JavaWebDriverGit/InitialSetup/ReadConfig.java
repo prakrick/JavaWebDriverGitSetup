091e00140a2f1272d77bd16e373579f2314f29a3
prakrick <prakrick@gmail.com>
2017-03-15 22:26:54 +0530

    updated by Vaibhav

--- src/com/JavaWebDriverGit/InitialSetup/ReadConfig.java
+++ src/com/JavaWebDriverGit/InitialSetup/ReadConfig.java
@@ -1,5 +1,6 @@
 package com.JavaWebDriverGit.InitialSetup;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,40 +8,44 @@ import java.util.Date;
 import java.util.Properties;
 
 public class ReadConfig {
-//https://www.slideshare.net/eneldoserrata/git-49020118
+	//https://www.slideshare.net/eneldoserrata/git-49020118
 	//https://git-scm.com/book/en/v2/
 	//https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
 	String result = "";
-	InputStream inputStream;
- 
+	static String projPath = System.getProperty("user.dir");
+	FileInputStream fis;
+
+	//InputStream inputStream;
+
 	public String getPropValues() throws IOException {
- 
+
 		try {
 			Properties prop = new Properties();
 			String propFileName = "config.properties";
- 
-			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
- 
-			if (inputStream != null) {
-				prop.load(inputStream);
+
+			//inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+			fis = new FileInputStream(projPath + "/resources/config.properties");
+
+			if (fis != null) {
+				prop.load(fis);
 			} else {
 				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
 			}
- 
+
 			Date time = new Date(System.currentTimeMillis());
- 
+
 			// get the property value and print it out
 			String browser = prop.getProperty("browser");
 			String url = prop.getProperty("url");
-			
+
 			result = "Browser = " + browser + ", URL = " + url;
 			System.out.println(result + "\nProgram Ran on " + time);
 		} catch (Exception e) {
 			System.out.println("Exception: " + e);
 		} finally {
-			inputStream.close();
+			fis.close();
 		}
 		return result;
 	}
-		
-	}
+
+}
