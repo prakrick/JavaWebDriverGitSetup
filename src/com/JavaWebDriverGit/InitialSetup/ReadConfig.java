e1a8b88b17a7df2b7acb41fb78e142ea1c05df03
prakrick <prakrick@gmail.com>
2017-05-02 16:54:00 +0530

    test
    maven cucumber

--- src/com/JavaWebDriverGit/InitialSetup/ReadConfig.java
+++ src/com/JavaWebDriverGit/InitialSetup/ReadConfig.java
@@ -3,7 +3,6 @@ package com.JavaWebDriverGit.InitialSetup;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Date;
 import java.util.Properties;
 
--- src/com/JavaWebDriverGit/TestScript/TestScripts.java
+++ src/com/JavaWebDriverGit/TestScript/TestScripts.java
@@ -8,6 +8,7 @@ public class TestScripts {
 	public static void main(String[] args) throws IOException {
 		ReadConfig readConfigObj = new ReadConfig();
 		readConfigObj.getPropValues();
+		System.out.println("merge check");
 	}
 
 }
