918bf2f767ad8343fb74c8434c8b671d093772b9
guptap <guptap@192.168.156.96>
2017-05-26 15:55:48 +0530

    test case run success CUCUMBER

--- src/com/JavaWebDriverGit/TestScript/TestRunner.java
+++ src/com/JavaWebDriverGit/TestScript/TestRunner.java
@@ -3,15 +3,13 @@ package com.JavaWebDriverGit.TestScript;
 
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
-import cucumber.api.PendingException;
-import cucumber.api.java.en.Given;
 import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "Feature"
-	,glue={"stepDefinition"}
+	,glue={"com.JavaWebDriverGit.TestScript"}
 		)
 public class TestRunner {
 
